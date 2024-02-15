a0d1eaa30f390711894293296c4c10db372c5d20
lukovkin-a-volsu <prib-222_676138@volsu.ru>
2024-02-15 14:45:32 +0300

    add tests, disclose conditions

--- Laboratory-Report-1/main.cpp
+++ Laboratory-Report-1/main.cpp
@@ -5,22 +5,91 @@ using std::endl;
 
 template <class T, int n>
 int is_monotonous(T data);
+void test();
 
 int main()
 {
-  int a[] = {1,
-             2,
-             2};
-
-  int res = is_monotonous<int *, 3>(a);
-  cout << res << endl;
+  test();
   return 0;
+}
+
+void test()
+{
+  int *a;
+
+  a = new int[3]{1,
+                 2,
+                 2};
+  cout << is_monotonous<int *, 3>(a)
+       << endl; // 1
+  delete[] a;
+
+  a = new int[3]{1,
+                 2,
+                 3};
+  cout << is_monotonous<int *, 3>(a)
+       << endl; // 1
+  delete[] a;
+
+  a = new int[3]{2,
+                 2,
+                 2};
+  cout << is_monotonous<int *, 3>(a)
+       << endl; // 0
+  delete[] a;
+
+  a = new int[3]{3,
+                 2,
+                 2};
+  cout << is_monotonous<int *, 3>(a)
+       << endl; // -1
+  delete[] a;
+
+  a = new int[3]{3,
+                 2,
+                 1};
+  cout << is_monotonous<int *, 3>(a)
+       << endl; // -1
+  delete[] a;
+
+  double *b;
+
+  b = new double[5]{
+      1,
+      2.3,
+      2.3,
+      2.4,
+      2.6};
+  cout << is_monotonous<double *, 5>(b)
+       << endl; // 1
+  delete[] b;
+
+  b = new double[5]{
+      1,
+      2.3,
+      2.3,
+      2.2,
+      2.4};
+  cout << is_monotonous<double *, 5>(b)
+       << endl; // 0
+  delete[] b;
+
+  b = new double[5]{
+      3,
+      2.3,
+      2.2,
+      2.1,
+      1.4};
+  cout << is_monotonous<double *, 5>(b)
+       << endl; // -1
+  delete[] b;
 }
 
 template <class T, int n>
 int is_monotonous(T data)
 {
-  int increase, decrease = 0;
+  int increase = 0, decrease = 0;
+
   for (int i = 1; i < n; i++)
   {
     if (data[i] > data[i - 1])
@@ -33,9 +102,21 @@ int is_monotonous(T data)
     }
 
     if (increase != 0 && decrease != 0)
+    {
       return 0;
+    }
   }
 
-  return (increase == 0 && decrease == 0) ? 0 : increase > 0 ? 1
-                                                             : -1;
+  if (increase == 0 && decrease == 0)
+  {
+    return 0;
+  }
+  else if (increase > 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return -1;
+  }
 }
