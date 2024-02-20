c62030201bacdc6f1f7dafb47350144643340f74
lukovkin-a-volsu <prib-222_676138@volsu.ru>
2024-02-20 21:36:38 +0300

    add tests

--- Laboratory-Report-2/main.cpp
+++ Laboratory-Report-2/main.cpp
@@ -1,5 +1,6 @@
-#include <random>
 #include <stdlib.h>
+#include <random>
+#include <iomanip>
 #include "algorithm.h"
 
 #ifndef RAND_MIN
@@ -11,35 +12,105 @@
 
 using std::cout;
 using std::endl;
+using std::fixed;
+using std::setprecision;
+
+std::default_random_engine generator(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+// T - array type, n - array size
+template <class T, int n>
+void test(int times, double &ms);
 
 template <class T, int n>
 T *rand_arr();
+
 template <class T, int n>
 void print_arr(T *arr);
 
 int main()
 {
-    double *arr = rand_arr<double, 10>();
-    print_arr<double, 10>(arr);
-    cout << format_out(is_monotonous<double *, 10>(arr)) << endl;
+    double avg_ms;
+    double max_avg_ms = 0;
+    int times = 1000;
 
+    // 1
+    test<double, 1000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 2
+    test<double, 2000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 3
+    test<double, 5000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 4
+    test<double, 10000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 5
+    test<double, 20000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 6
+    test<double, 50000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 7
+    test<double, 100000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 8
+    test<double, 200000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    // 9
+    test<double, 500000>(times, avg_ms);
+    cout << fixed << setprecision(10) << avg_ms << " ms." << endl;
+    max_avg_ms += avg_ms;
+    
+    cout << fixed << setprecision(10) << max_avg_ms << " max ms." << endl;
     return 0;
+}
+
+template <class T, int n>
+void test(int times, double &avg_ms)
+{
+    avg_ms = 0;
+    for (int i = 0; i < times; i++)
+    {
+        double ms = 0;
+        T *arr = rand_arr<T, n>();
+        is_monotonous<T, n>(arr, ms);
+        avg_ms += ms;
+    }
+    avg_ms /= times;
 }
 
 template <class T, int n>
 T *rand_arr()
 {
     double random;
-    std::mt19937 gen;
-    std::uniform_real_distribution<> dist(RAND_MIN, RAND_MAX);
-    T *array = new T[n];
+    // compile generator
+    // std::mt19937 generator;
+    std::uniform_real_distribution<double> dist(RAND_MIN, RAND_MAX);
+    T *arr = new T[n];
     for (int i = 0; i < n; i++)
     {
-        random = dist(gen);
-        T value = (T)random;
-        array[i] = value;
+        random = dist(generator);
+        T val = (T)random;
+        arr[i] = val;
     }
-    return array;
+    return arr;
 }
 
 template <class T, int n>
