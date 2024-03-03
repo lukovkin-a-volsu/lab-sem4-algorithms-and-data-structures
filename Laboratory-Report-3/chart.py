2e3326b7d81c8f0fa59761affe67394ef0135dc4
lukovkin-a-volsu <prib-222_676138@volsu.ru>
2024-03-03 16:45:49 +0300

    fix labels

--- Laboratory-Report-3/chart.py
+++ Laboratory-Report-3/chart.py
@@ -8,9 +8,9 @@ data = np.loadtxt('output.txt', delimiter=';')
 x = data[:, 0]
 y = data[:, 1]
 
-ax.set_title('T(n), T - time in s., n - array size')
+ax.set_title('T(n), T - time in ms., n - array size')
 ax.set_xlabel('n')
-ax.set_ylabel('s.')
+ax.set_ylabel('ms.')
 
 ax.plot(x, y,)
 plt.show()
